2d6378e86b2c2d75acb58ca3faf3262d363806ab
ALA EDDINE <alaeddinebenhassir@gmail.com>
2018-08-30 00:08:53 +0000

    Update pyget.py

--- pyget.py
+++ pyget.py
@@ -4,14 +4,10 @@ SERVER = ['10.1.0.100']
 sites = fetch("fetch.txt")
 KEYS = ['24.0.0.1']
 
-##COOP =['23.0.0.1']
-
-##COOP_ADD =['23.0.0.1']
-
 transform_set =''
 
 ipsec_profile =''
-#configuring key server 
+
 TN = login(SERVER[0])
 
 phase_1(TN ,sites)
@@ -33,38 +29,6 @@ TN.write(b"exit" + b"\n")
 a = TN.read_all()
 print(a.decode('utf-8'))
 
-#configuring coop server 
-
-
-##TN = login(SERVER[0])
-
-##exportkey(TN ,lable)
-
-
-
-##CP = login(COOP[0])
-
-##importkey(CP , lable)
-
-## CP = login(COOP[0])
-
-
-##phase_1(CP ,KEYS)
-
-##trans(CP ,transform_set)
-
-##ipsecprofile(CP , transform_set , ipsec_profile)
-
-##accessListe(CP)
-
-##GETVPNgroup(CP ," 11111 " ,COOP[0] ,ipsec_profile ,acl ,lable ,COOP_ADD[0])
-
-##Cmap(CP ,groupe) 
-
-#ADDING REDENDANCY 
-#redendancy(SERVER[0] ,COOP[0] , groupe)
-
-#Configuring GMs
 
 for site in sites :
     GM = login(site)
